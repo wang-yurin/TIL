3451bdd9361548af49f242cf9d475e4464ca9b3c
wang-yurin <king_1994@naver.com>
2023-01-02 01:40:27 +0900

    docs: 클래스형 컴포넌트, 함수형 컴포넌트 (3장)

--- React/04/src/App.js
+++ React/04/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div>App</div>;
+// 함수형 컴포넌트
+// function App() {
+//   const name = '왕';
+//   return <div>나는 {name} </div>;
+// }
+
+// export default App;
+
+// 클래스형 컴포넌트
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const name = '왕';
+    return <div>나는 {name}</div>;
+  }
 }
 
 export default App;
